f9cce4d3a8d90127ab0d5c3082f25509c9972ce2
LIU CHUNKAI <traderkai619@gmail.com>
2026-06-20 09:38:04 +0900

    Replace EmptyOperator with BashOperator for tasks

--- project_c/dags/project_c_pipeline.py
+++ project_c/dags/project_c_pipeline.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash import BashOperator
 from datetime import datetime
 
 with DAG(
@@ -10,32 +10,39 @@ with DAG(
     tags=["dbt", "analytics-engineering", "project-c"],
 ) as dag:
 
-    stg_fact_sales = EmptyOperator(
-        task_id="stg_fact_sales"
+    stg_fact_sales = BashOperator(
+        task_id="stg_fact_sales",
+        bash_command="cd project_c && dbt run --select stg_fact_sales"
     )
 
-    int_sales_metrics = EmptyOperator(
-        task_id="int_sales_metrics"
+    int_sales_metrics = BashOperator(
+        task_id="int_sales_metrics",
+        bash_command="cd project_c && dbt run --select int_sales_metrics"
     )
 
-    mart_daily_revenue = EmptyOperator(
-        task_id="mart_daily_revenue"
+    mart_daily_revenue = BashOperator(
+        task_id="mart_daily_revenue",
+        bash_command="cd project_c && dbt run --select mart_daily_revenue"
     )
 
-    mart_reconciliation_check = EmptyOperator(
-        task_id="mart_reconciliation_check"
+    mart_reconciliation_check = BashOperator(
+        task_id="mart_reconciliation_check",
+        bash_command="cd project_c && dbt run --select mart_reconciliation_check"
     )
 
-    mart_anomaly_detection = EmptyOperator(
-        task_id="mart_anomaly_detection"
+    mart_anomaly_detection = BashOperator(
+        task_id="mart_anomaly_detection",
+        bash_command="cd project_c && dbt run --select mart_anomaly_detection"
     )
 
-    mart_data_trust_score = EmptyOperator(
-        task_id="mart_data_trust_score"
+    mart_data_trust_score = BashOperator(
+        task_id="mart_data_trust_score",
+        bash_command="cd project_c && dbt run --select mart_data_trust_score"
     )
 
-    dbt_tests = EmptyOperator(
-        task_id="dbt_tests"
+    dbt_tests = BashOperator(
+        task_id="dbt_tests",
+        bash_command="cd project_c && dbt test"
     )
 
     (
